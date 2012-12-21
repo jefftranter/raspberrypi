098ec6e1f94f84f4b583b5f8c77c149ab3c0b3d6
Jeff Tranter <tranter@pobox.com>
2012-12-20 22:52:31 -0500

    More packaging work.

---
+++ adventure/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp adventure /usr/bin/adventure
+cp adventure.sh /usr/bin/adventure.sh
+cp adventure16x16.png /usr/share/icons/hicolor/16x16/adventure.png
+cp adventure32x32.png /usr/share/icons/hicolor/32x32/adventure.png
+cp adventure64x64.png /usr/share/icons/hicolor/64x64/adventure.png
+cp adventure.desktop /usr/share/applications/adventure.desktop
---
+++ adventure/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm /usr/bin/adventure
+rm /usr/bin/adventure.sh
+rn /usr/share/icons/hicolor/16x16/adventure.png
+rm /usr/share/icons/hicolor/32x32/adventure.png
+rm /usr/share/icons/hicolor/64x64/adventure.png
+rm /usr/share/applications/adventure.desktop
