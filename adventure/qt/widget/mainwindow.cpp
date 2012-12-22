a0981dc7e645e53e619fe52e5d2d9b1110b51d27
Jeff Tranter <tranter@pobox.com>
2012-12-22 10:10:37 -0500

    Implement more of UI.

--- adventure/qt/widget/mainwindow.cpp
+++ adventure/qt/widget/mainwindow.cpp
@@ -6,6 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    locationLabel = new QLabel(tr("Turns: 60"));
+    turnsLabel = new QLabel(tr("Location: driveway"));
+    statusBar()->addPermanentWidget(turnsLabel, 1);
+    statusBar()->addPermanentWidget(locationLabel);
+
+    ui->textEdit->setText(
+"                        Abandoned Farmhouse Adventure\n"
+"                                By Jeff Tranter\n"
+"\n"
+"Your three-year-old grandson has gone missing and was last seen headed in the\n"
+"direction of the abandoned family farm. It's a dangerous place to play.\n"
+"You have to find him before he gets hurt, and it will be getting dark soon...\n"
+"? "
+);
+
 }
 
 MainWindow::~MainWindow()
--- adventure/qt/widget/mainwindow.h
+++ adventure/qt/widget/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,8 @@ public:
     
 private:
     Ui::MainWindow *ui;
+    QLabel *locationLabel;
+    QLabel *turnsLabel;
 };
 
 #endif // MAINWINDOW_H
