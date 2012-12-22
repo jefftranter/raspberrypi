5fa8f377f3c91138560df8cbfcbd693bf5f34d0e
Jeff Tranter <tranter@pobox.com>
2012-12-21 19:56:52 -0500

    Fix install issue.

--- adventure/install.sh
+++ adventure/install.sh
@@ -5,3 +5,5 @@ cp afadv16x16.png /usr/share/icons/hicolor/16x16/afadv.png
 cp afadv32x32.png /usr/share/icons/hicolor/32x32/afadv.png
 cp afadv64x64.png /usr/share/icons/hicolor/64x64/afadv.png
 cp afadv.desktop /usr/share/applications/afadv.desktop
+chmod a+x /usr/bin/afadv
+chmod a+x /usr/bin/afadv.sh
