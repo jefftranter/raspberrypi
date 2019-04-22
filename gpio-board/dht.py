3b8c88a8308a8cb9c88fac27b7e01baa23d68302
Jeff Tranter <tranter@pobox.com>
2019-04-21 22:01:30 -0400

    Fix bug in script.

--- gpio-board/dht.py
+++ gpio-board/dht.py
@@ -9,9 +9,8 @@ sensor = 22
 # Sensor GPIO pin
 pin = 26
 
-humidity, ctemp = Adafruit_DHT.read_retry(sensor, pin)
-
 while True:
+  humidity, ctemp = Adafruit_DHT.read_retry(sensor, pin)
   if humidity is not None and ctemp is not None:
     ftemp = ctemp * 9/5.0 + 32
     print('Temperature: {0:0.1f}C/{1:0.1f}F Humidity: {2:0.1f}%'.format(ctemp, ftemp, humidity))
